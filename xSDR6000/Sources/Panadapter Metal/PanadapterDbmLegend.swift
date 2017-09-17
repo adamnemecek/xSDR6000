2fd6a7720c5716ec3748dbc10fbeaad489f29f9d
Douglas Adams <Doug@dougs-macbook.local>
2017-09-17 10:25:01 -0400

    Added Panadapter FrequencyLegend & DbmLegend views

---
+++ xSDR6000/Sources/Panadapter Metal/PanadapterDbmLegend.swift
@@ -0,0 +1,26 @@
+//
+//  PanadapterDbmLegend.swift
+//  xSDR6000
+//
+//  Created by Douglas Adams on 9/16/17.
+//  Copyright © 2017 Douglas Adams. All rights reserved.
+//
+
+
+import Cocoa
+import SwiftyUserDefaults
+
+// --------------------------------------------------------------------------------
+// MARK: - Panadapter Dbm Legend View class implementation
+// --------------------------------------------------------------------------------
+
+final class PanadapterDbmLegend: NSView {
+    
+    
+    // ----------------------------------------------------------------------------
+    // MARK: - Overridden methods
+    
+    override func awakeFromNib() {
+        
+    }
+}
---
+++ xSDR6000/Sources/Panadapter Metal/PanadapterFrequencyLegend.swift
@@ -0,0 +1,25 @@
+//
+//  PanadapterFrequencyLegend.swift
+//  xSDR6000
+//
+//  Created by Douglas Adams on 9/16/17.
+//  Copyright © 2017 Douglas Adams. All rights reserved.
+//
+
+import Cocoa
+import SwiftyUserDefaults
+
+// --------------------------------------------------------------------------------
+// MARK: - Panadapter Frequency Legend View class implementation
+// --------------------------------------------------------------------------------
+
+final class PanadapterFrequencyLegend: NSView {
+
+    
+    // ----------------------------------------------------------------------------
+    // MARK: - Overridden methods
+    
+    override func awakeFromNib() {
+        
+    }
+}
