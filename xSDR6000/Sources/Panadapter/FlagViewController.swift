384e371babf39cabf782f84331337cdc40474c4f
Jacqui Adams <jacqad@icloud.com>
2017-10-22 12:28:13 -0400

    Under construction

---
+++ xSDR6000/Sources/Panadapter/FlagViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FlagViewController.swift
+//  xSDR6000
+//
+//  Created by Douglas Adams on 10/22/17.
+//  Copyright © 2017 Douglas Adams. All rights reserved.
+//
+
+import Cocoa
+
+class FlagViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
